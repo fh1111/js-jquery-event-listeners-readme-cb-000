137379c84f08a07227d1b3563928a173cd84006c
Fer Hernandez <fh.1111@gmail.com>
2019-02-06 02:46:12 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,6 +15,21 @@ $(document).ready(function(){
     })
   }
 
+  function pressIt() {
+    $('#typing').on('keydown', function(e) {
+      // 'g' corresponds to 71
+      if (e.which === 71) {
+        alert("You pressed 'g'!")
+      }
+    })
+  }
+  
+  function submitIt() {
+    $('form').on('submit', function(e) {
+      alert('Your form is going to be submitted now.')
+    })
+  }
+
 
 
 
